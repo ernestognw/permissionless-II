e34bea52bc00adecd46caca0901fc2213c0e100b
ernestognw <ernestognw@gmail.com>
2023-09-11 00:26:31 -0500

    First commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,30 @@
+import { ethers, defender } from "hardhat";
+
+async function main() {
+  const Box = await ethers.getContractFactory("Box");
+
+  const defaultApprovalProcess = await defender.getDefaultApprovalProcess();
+
+  if (defaultApprovalProcess.address === undefined) {
+    throw new Error(
+      `Upgrade approval process with id ${defaultApprovalProcess.approvalProcessId} has no assigned address`
+    );
+  }
+
+  const deployment = await defender.deployProxy(
+    Box as any,
+    [5, defaultApprovalProcess.address],
+    { initializer: "initialize" }
+  );
+
+  await deployment.waitForDeployment();
+
+  console.log(`Contract deployed to ${await deployment.getAddress()}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
